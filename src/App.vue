0e6757e438782af813bd4d9e9d489f6577c7f08c
Danielle Sheehan <danibsheehan@gmail.com>
2019-11-19 20:34:16 -0500

    getting project started

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="We got a message going!"/>
   </div>
 </template>
 
