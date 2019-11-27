fef8151f34dac121e6bfb44b6d59c3796576aeb7
Danielle Sheehan <danibsheehan@gmail.com>
2019-11-27 12:29:10 -0500

    add axios extenions

--- src/http-common.js
+++ src/http-common.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
+import { cacheAdapterEnhancer } from 'axios-extensions';
 
 const http = axios.create({
 	baseURL: `https://api.sportsdata.io/v3/mlb/scores/json/`,
 	params: {
 		'key': process.env.VUE_APP_SPORTS_KEY
-	}
+	},
+	adapter: cacheAdapterEnhancer(axios.defaults.adapter)
 });
 
 export default http;
