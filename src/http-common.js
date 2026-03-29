4d042ccdbeb697a20e1cc51d41327ec9b0799205
Danielle Sheehan <danibsheehan@gmail.com>
2026-03-29 17:56:23 -0400

    update axios adapter

--- src/http-common.js
+++ src/http-common.js
@@ -1,11 +1,15 @@
-import axios from 'axios';
+import axios, { getAdapter } from 'axios';
 import { cacheAdapterEnhancer } from 'axios-extensions';
 
 const apiBase = process.env.VUE_APP_API_BASE || '';
 
+// Axios 1.x uses adapter name list on defaults, not a single function; cacheAdapterEnhancer needs a callable.
+const defaultAdapter = (config) =>
+	getAdapter(axios.defaults.adapter, config)(config);
+
 const http = axios.create({
 	baseURL: apiBase || (typeof location !== 'undefined' ? location.origin : ''),
-	adapter: cacheAdapterEnhancer(axios.defaults.adapter)
+	adapter: cacheAdapterEnhancer(defaultAdapter)
 });
 
 export default http;
